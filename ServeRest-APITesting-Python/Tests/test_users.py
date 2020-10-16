c38e9d625eb93cd3d29fb90d686b24bcf4dec690
Ketlin Pedron <pedron.ketlin@gmail.com>
2020-10-15 21:38:57 -0400

    Update user tests
    Signed-off-by: Ketlin Pedron <pedron.ketlin@gmail.com>

--- ServeRest-APITesting-Python/Tests/test_users.py
+++ ServeRest-APITesting-Python/Tests/test_users.py
@@ -14,7 +14,7 @@ class Users(unittest.TestCase):
         response = requests.get(url=full_url)
 
         # Check the response from ServeRest
-        assert response.status_code == 200
+        self.assertEqual(response.status_code, 200, "Error in status code to get all user")
 
     def test_create_common_user(self):
         full_url = BASE_URL + "/usuarios"
@@ -27,19 +27,53 @@ class Users(unittest.TestCase):
         response = requests.post(url=full_url, json=body)
 
         # Check the response from ServeRest
-        assert response.status_code == 201
+        self.assertEqual(response.status_code, 201, "Error in status code to create an user")
 
         response_json = json.loads(response.text)
-        assert response_json["message"] == "Cadastro realizado com sucesso"
+        self.assertEqual(response_json["message"], "Cadastro realizado com sucesso")
         user_id = response_json["_id"]
 
         # It is necessary to remove this user, this is
         # a good test automation practice :)
         full_url_del = full_url + "/" + user_id
         response = requests.delete(url=full_url_del)
+        self.assertEqual(response.status_code, 200, "Error in status code to delete a user")
 
-        assert response.status_code == 200
         response_json = json.loads(response.text)
-        assert response_json["message"] == "Registro excluído com sucesso"
+        self.assertEqual(response_json["message"], "Registro excluído com sucesso")
 
+    def test_update_user_info(self):
+        full_url = BASE_URL + "/usuarios"
+        body = {"nome": "Ketlin Pedron",
+                "email": "kpedron@qa.com",
+                "password": "t1234",
+                "administrador": "false"}
 
+        # Send HTTP Request
+        response = requests.post(url=full_url, json=body)
+        response_json = json.loads(response.text)
+        user_id = response_json["_id"]
+
+        # Check the response from ServeRest
+        self.assertEqual(response.status_code, 201, "Error in status code to create an user")
+
+        # Now we will update information (email + password)
+        full_url = BASE_URL + "/usuarios/" + user_id
+        new_body = {"nome": "Ketlin Pedron",
+                    "email": "newemail@qa.com",
+                    "password": "passwd123",
+                    "administrador": "false"}
+
+        # Send HTTP Request
+        response = requests.put(url=full_url, json=new_body)
+        self.assertEqual(response.status_code, 200, "Error in status to update an user information")
+
+        response_json = json.loads(response.text)
+        self.assertEqual(response_json["message"],  "Registro alterado com sucesso")
+
+        # It is necessary to remove this user, this is
+        # a good test automation practice :)
+        full_url_del = BASE_URL + "/usuarios/" + user_id
+        response = requests.delete(url=full_url_del)
+
+        self.assertEqual(response.status_code, 200, "Error in status code to delete a user")
